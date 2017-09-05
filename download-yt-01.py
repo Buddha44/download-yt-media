4872a30913d2adab6f5fcc59880cd329d71cf8fa
Jesus A Cunrad <31639352+Buddha44@users.noreply.github.com>
2017-09-05 03:56:36 +0100

    Create download-yt-01.py
    "Hello World!" intro

---
+++ download-yt-01.py
@@ -0,0 +1,7 @@
+
+mesg1 = """
+I want to create script(s) to help download some various types of the media, available at youtube. 
+The python script would wrap around 'youtube-dl' CLI app.
+"""
+
+print(mesg1)
